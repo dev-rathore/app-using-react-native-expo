f770e548d00863bd6e5f588044ea2cb6dd38b1db
dev-rathore <rathoredvd3001@gmail.com>
2024-07-02 22:48:33 +0530

    project setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="testing"
+        options={{
+          title: 'Testing',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/testing.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function TabThreeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text style={{ color: '#FFFFFF' }}>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
