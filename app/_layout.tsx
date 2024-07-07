832702adbcf36434fcf73fbde9101041ce16e673
dev-rathore <rathoredvd3001@gmail.com>
2024-07-07 15:07:51 +0530

    root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Alert } from 'react-native';
+import { useAuthStore } from '@/store/auth-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -15,6 +18,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const router = useRouter();
 
   useEffect(() => {
     if (loaded) {
@@ -22,16 +26,44 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const storedToken = await AsyncStorage.getItem('token');
+        if (storedToken) {
+          useAuthStore.setState({ token: storedToken });
+          router.replace('/(drawer)/(tabs)/home');
+        } else {
+          router.replace('/');
+        }
+      } catch (error) {
+        Alert.alert('Error', 'Failed to retrieve token. Please try again later.');
+      }
+    };
+
+    checkToken();
+  }, []);
+
   if (!loaded) {
     return null;
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack
+        screenOptions={{
+          animation: 'ios',
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(drawer)"
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
   );
-}
+};
