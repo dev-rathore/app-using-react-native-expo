13f5f081e73bc9832f65ff61aa12137f4e25ef0f
dev-rathore <rathoredvd3001@gmail.com>
2024-07-07 15:29:48 +0530

    app layout background color

--- components/app-layout/app-layout.tsx
+++ components/app-layout/app-layout.tsx
@@ -1,21 +1,27 @@
 import { PropsWithChildren } from "react";
 import { Platform, SafeAreaView, StatusBar } from "react-native";
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { Colors } from "@/constants/Colors";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 const AppLayout: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   const isAndroid = Platform.OS === "android";
+  const lightColor = Colors.light.background;
+  const darkColor = Colors.dark.background;
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        padding: isAndroid ? StatusBar.currentHeight : 0,
+        backgroundColor: backgroundColor,
+        paddingTop: isAndroid ? StatusBar.currentHeight : 0,
       }}
     >
       {children}
-      <ExpoStatusBar style="auto" />
+      <ExpoStatusBar style="light" />
     </SafeAreaView>
   );
 };
