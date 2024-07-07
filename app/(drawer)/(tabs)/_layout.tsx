2f0363ff67b398fb25c23fd95e43c2912666ad5a
dev-rathore <rathoredvd3001@gmail.com>
2024-07-07 15:19:29 +0530

    tabs layout

---
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -0,0 +1,66 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveBackgroundColor: Colors['dark'].background,
+        tabBarActiveTintColor: Colors['dark'].tint,
+        tabBarInactiveBackgroundColor: Colors['dark'].background,
+        tabBarStyle: {
+          backgroundColor: Colors['dark'].background,
+        },
+      }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'airplane' : 'airplane-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="reel"
+        options={{
+          title: 'Reel',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'film' : 'film-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'settings' : 'settings-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
